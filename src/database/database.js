80d30cf5815014f9c0af59de09c91d02c308296a
duxio14 <111642162+duxio14@users.noreply.github.com>
2022-12-29 18:57:07 +0100

    Update database.js

--- src/database/database.js
+++ src/database/database.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-    host: "bddduxia.axial-host.net", 
-    user: "u1_cFF4un0ypT",
-    password: "vV0i5QW@n+feDPPk8cHX1GMC",
-    database: "s1_akuno", 
+    host: "", 
+    user: "",
+    password: ",
+    database: "", 
 });
 
 module.exports = db;
